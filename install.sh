8a216d3f61b45abc5ed01ad92d7c6fd1d0d2ad28
Brandon Davis <git@subdavis.com>
2020-12-02 21:11:07 -0500

    everything

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SYSTEMD_DIR=${SYSTEMD_DIR:-/usr/local/lib/systemd/system}
+files=("media-primary.mount" "media-secondary.mount")
+
+systemctl daemon-reload
+
+for file in "${files[@]}"
+do
+	ln --symbolic --force --no-dereference $(pwd)/${file} ${SYSTEMD_DIR}/${file}
+	systemctl enable $file
+    systemctl restart $file
+done
+
+ln --symbolic --force --no-dereference $(pwd)/etc/traefik-logrotate.conf /etc/logrotate.d/traefik
